8f96ffdc44b546c4fa5f01abc5c0fd655f628aa6
koryunkoryun025-cmd <koryun.koryun025@gmail.com>
2026-06-24 10:15:55 +0400

    update app.prop

--- src/main/java/com/marv/taskmanagement/config/DataInitializer.java
+++ src/main/java/com/marv/taskmanagement/config/DataInitializer.java
@@ -4,11 +4,14 @@ import com.marv.taskmanagement.model.entity.UserEntity;
 import com.marv.taskmanagement.model.enums.Role;
 import com.marv.taskmanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements ApplicationRunner {
@@ -16,15 +19,22 @@ public class DataInitializer implements ApplicationRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${app.admin.username}")
+    private String adminUsername;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
     @Override
     public void run(ApplicationArguments args) {
-        if (!userRepository.existsByUsername("admin")) {
+        if (!userRepository.existsByUsername(adminUsername)) {
             UserEntity admin = new UserEntity();
-            admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("admin987654321"));
+            admin.setUsername(adminUsername);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
             admin.setRole(Role.ROLE_ADMIN);
             userRepository.save(admin);
-            System.out.println("Admin created: admin / admin987654321");
+
+            log.info("Default admin account created");
         }
     }
 }
